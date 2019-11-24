5142b574077a7c6bee46090a01d7dccf10fd11a1
Talion97 <55638572+Talion97@users.noreply.github.com>
2019-11-24 13:30:20 +0200

    Update chess.php

--- chess.php
+++ chess.php
@@ -1,13 +1,13 @@
 <?php
 
 include 'class/Storage.php';
-// include 'class/FileStorage.php';
+include 'class/FileStorage.php';
 // include 'class/MysqlStorage.php';
-include 'class/SessionStorage.php';
+// include 'class/SessionStorage.php';
 include 'class/Board.php';
 
-$storage = new SessionStorage('map');
-// $storage = new FileStorage('figures.txt');
+// $storage = new SessionStorage('map');
+$storage = new FileStorage('figures.txt');
 // $storage = new MysqlStorage('mysql:host=localhost;dbname=CHESS;charset=utf8', 'root', '');
 $board = new Board($storage);
 
@@ -19,15 +19,3 @@ if (isset($_GET['getFigures']))
 
 if (isset($_GET['moveFigure']))
     echo $board->moveFigure($_GET['frCoord'], $_GET['toCoord']);
-
-
-// if (isset($_GET['moveFigure'])) {
-//     $frCoord = $_GET['frCoord'];
-//     $toCoord = $_GET['toCoord'];
-//     $figures =$storage->load();
-//     $figure = $figures[$frCoord];
-//     $figures[$frCoord]='1';
-//     $figures[$toCoord]=$figure;
-//     $storage->save($figure);
-//     echo $storage->load();
-// }
